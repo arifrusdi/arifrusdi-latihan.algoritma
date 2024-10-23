339ef5806a2e15c3236d8523434afba0278dfb25
arifrusdi <muhammadarif98427@gmail.com>
2024-10-23 08:29:17 +0800

    Add files via upload

---
+++ perhitungan aritmatika.py
@@ -0,0 +1,20 @@
+#Penambahan
+a = 90
+j = 80
+hasil = a + j
+print (hasil)
+#Pengurangan
+z = 50
+y = 30
+hasil = z - y
+print (hasil)
+#Perkalian
+u = 10
+i = 60
+hasil = u * i
+print (hasil)
+#Pembagian
+o = 80
+p = 20
+hasil = o / p
+print (hasil)

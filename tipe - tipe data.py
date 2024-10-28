8dd983159073cc88ddb34e5ff5b1f2994c60f4fc
arifrusdi <muhammadarif98427@gmail.com>
2024-10-28 21:54:37 +0800

    Add files via upload

---
+++ tipe - tipe data.py
@@ -0,0 +1,35 @@
+# Tipe data string
+a = "abdul sepupu saya"
+print (a)
+
+# Tipe data integer
+i = 45
+print (i)
+
+# Tipe data float
+o = 31,6
+print (o)
+
+# Tipe data complex Number
+x = 7j
+print (x)
+
+# Tipe data boolean
+n = False
+print (n)
+
+# Tipe data List
+rifff = ["enam" , "tujuh" , "delapan"]
+print (rifff)
+
+# Tipe data Tuple
+han = ("delapan" , "sembilan" , "sepuluh")
+print (han)
+
+#Tipe data set
+rifff = {"dua" , "tiga" , "empat"}
+print (rifff)
+
+# Tipe data dictionary
+arifff = {"sebelas" : 11 , "dua belas" : 12.}
+print (arifff)
